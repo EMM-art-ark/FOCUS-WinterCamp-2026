c2c6181f5bd363b5a29c056a552d5d9d11884a1a
EMM-art-ark <197692568@qq.com>
2026-01-27 16:55:42 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
 from ultralytics import YOLO
 test = YOLO(model='runs/detect/train8/weights/best.pt', task='detect')
-result = test(source='xr.jpg', save=True)
+
+result = test(source='test1.jpg', save=True)
